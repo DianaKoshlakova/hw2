c302fdb93d2234ce5a8a8760c0fadc277666f176
DianaKoshlakova <di.koshlakova.nkv@yandex.ru>
2023-05-24 09:59:25 +0300

    com

--- 10.py
+++ 10.py
@@ -9,14 +9,13 @@ n = int(input("Введите число n: "))
 countG = 0 # гербы
 countR = 0 # решки
 for i in range(n):
-    x = int(input("Если решка, введите 1, если герб - 2: "))
+    x = int(input("Если решка, введите 1, если герб - 0: "))
     if x == 1 :
         countR +=1
-    elif x == 2 :
+    elif x == 0 :
         countG +=1
-    else:
-        print("Запрос введен некорректно")
-        break
+    else :
+        print("Запрос введён некорректно!")
 if countG >= countR :
     print(f"Нужно перевернуть {countR}")
 else :
